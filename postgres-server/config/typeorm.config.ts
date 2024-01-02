12791f97c0981b431b28df7144ea2e7fe893de2d
Mastered1738 <vinkomilotic632@gmail.com>
2024-01-02 15:18:42 +0100

    feat: changed to a more stable database

--- postgres-server/config/typeorm.config.ts
+++ postgres-server/config/typeorm.config.ts
@@ -1,6 +1,6 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-export const typeOrmConfig: TypeOrmModuleOptions = {
+/*export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: 'insight.cf8mu8ksyya3.eu-central-1.rds.amazonaws.com',
   port: 5432,
@@ -8,6 +8,18 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   username: 'Vinko',
   password: 'Vinko_324',
   schema: 'insight-schema',
+ // entities: [__dirname + '/../**/ //*.entity{.ts,.js}'],
+//synchronize: true,
+//autoLoadEntities: true,
+//};
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: 'postgresql-159670-0.cloudclusters.net',
+  port: 18290,
+  database: 'Insight',
+  username: 'Vinko',
+  password: 'Vinko_324',
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize: true,
   autoLoadEntities: true,
