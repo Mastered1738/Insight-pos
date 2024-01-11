60fffc708db14eb55b62a76d2ab4af2482f1660f
Mastered1738 <vinkomilotic632@gmail.com>
2024-01-11 19:04:33 +0100

    feat: add all modules to main app module

--- postgres-server/src/app.module.ts
+++ postgres-server/src/app.module.ts
@@ -9,6 +9,21 @@ import { SubjectPostModule } from './modules/subjects/subject-post.module';
 import { ProfilePostModule } from './modules/user/profile-post.module';
 import { PrivateMessageModule } from './modules/chat/private/private-message.module';
 import { ProfilePostCommentModule } from './modules/user/profile-post-comment.module';
+import { EventModule } from './modules/event/event.module';
+import { ShopItemsModule } from './modules/shop/shopItems.module';
+import { ShopTransactionsModule } from './modules/shop/shopTransactions.module';
+import { AnswerBoolModule } from './modules/test/answers/answerBool.module';
+import { AnswerOpenEndModule } from './modules/test/answers/answerOpenEnd.module';
+import { AnswerChooseOneModule } from './modules/test/answers/answerChooseOne.module';
+import { QuestionOpenEndModule } from './modules/test/questions/questionOpenEnd.module';
+import { QuestionBoolModule } from './modules/test/questions/questionBool.module';
+import { QuestionChooseOneModule } from './modules/test/questions/questionChooseOne.module';
+import { SubjectPostCommentModule } from './modules/subjects/subjectPostComment.module';
+import { TestModule } from './modules/test/test.module';
+import { TestTypeModule } from './modules/test/testType.module';
+import { TestScoreModule } from './modules/test/testScore.module';
+import { ServerGroupModule } from './modules/userServer/serverGroup.module';
+import { ServerGroupPostModule } from './modules/userServer/serverGroupPost.module';
 
 @Module({
   imports: [
@@ -21,6 +36,21 @@ import { ProfilePostCommentModule } from './modules/user/profile-post-comment.mo
     ProfilePostModule,
     PrivateMessageModule,
     ProfilePostCommentModule,
+    EventModule,
+    ShopItemsModule,
+    ShopTransactionsModule,
+    AnswerBoolModule,
+    AnswerOpenEndModule,
+    AnswerChooseOneModule,
+    QuestionBoolModule,
+    QuestionOpenEndModule,
+    QuestionChooseOneModule,
+    SubjectPostCommentModule,
+    TestModule,
+    TestTypeModule,
+    TestScoreModule,
+    ServerGroupModule,
+    ServerGroupPostModule,
   ],
   controllers: [],
   providers: [],
