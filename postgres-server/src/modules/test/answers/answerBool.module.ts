5e9c3a572f09625b49afb9543539ae2c18035074
Vinko Milotić <vinkomilotic632@gmail.com>
2024-01-13 08:40:05 +0100

    feat: add andswer module dependencies

--- postgres-server/src/modules/test/answers/answerBool.module.ts
+++ postgres-server/src/modules/test/answers/answerBool.module.ts
@@ -2,12 +2,17 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AnswerBoolController } from 'src/controllers/test/answers/answerBool.controller';
 import { AnswerBool } from 'src/entities/test/answer/answerBool.entity';
+import { AnswerChooseOne } from 'src/entities/test/answer/answerChooseOne.entity';
+import { AnswerOpenEnd } from 'src/entities/test/answer/answerOpenEnd.entity';
 import { AnswerBoolService } from 'src/providers/test/answers/answerBool.service';
+import { GlobalAnswerService } from 'src/providers/test/answers/answerGlobal.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AnswerBool])],
+  imports: [
+    TypeOrmModule.forFeature([AnswerBool, AnswerChooseOne, AnswerOpenEnd]),
+  ],
   controllers: [AnswerBoolController],
-  providers: [AnswerBoolService],
-  exports: [AnswerBoolService],
+  providers: [AnswerBoolService, GlobalAnswerService],
+  exports: [AnswerBoolService, GlobalAnswerService],
 })
 export class AnswerBoolModule {}
