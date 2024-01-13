538d3aaf274ead3e43168820b45171173bbc4c33
Vinko Milotić <vinkomilotic632@gmail.com>
2024-01-13 15:19:59 +0100

    feat: can create and get events by subjects

---
+++ postgres-server/src/dto/event/event.dto.ts
@@ -0,0 +1,7 @@
+export class EventDTO {
+  title: string;
+  description?: string;
+  start_time: Date;
+  end_time: Date;
+  subject_id: number;
+}
--- postgres-server/src/providers/event/event.service.ts
+++ postgres-server/src/providers/event/event.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { EventDTO } from 'src/dto/event/event.dto';
+import { Event } from 'src/entities/event/event.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class EventService {}
+export class EventService {
+  constructor(
+    @InjectRepository(Event) private eventRepository: Repository<Event>,
+  ) {}
+
+  async createEvent(eventDto: EventDTO): Promise<Event> {
+    return await this.eventRepository.save({
+      title: eventDto.title,
+      description: eventDto.description,
+      start_time: eventDto.start_time,
+      end_time: eventDto.end_time,
+      subject_id: eventDto.subject_id,
+    });
+  }
+
+  async getEventsBySubjects(subject_id: number): Promise<Event[]> {
+    return await this.eventRepository.find({
+      relations: ['subject'],
+      where: {
+        subject: {
+          subject_id: subject_id,
+        },
+      },
+    });
+  }
+}
