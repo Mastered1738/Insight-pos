62e46a8d4cbc1d336676a2828dc0f122fc61931c
Vinko Milotić <vinkomilotic632@gmail.com>
2024-01-15 15:10:58 +0100

    feat: add get private messages by user_id

--- postgres-server/src/controllers/chat/private/private-message.controller.ts
+++ postgres-server/src/controllers/chat/private/private-message.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { PrivateMessageService } from 'src/providers/chat/private/private-message.service';
+import { user_id_DTO } from '../../../dto/user/user_id.dto';
+import { PrivateMessage } from 'src/entities/chat/private/privateMessage.entity';
 
-@Controller('private-message')
+@Controller('private-messages')
 export class PrivateMessageController {
   constructor(private readonly messageService: PrivateMessageService) {}
+
+  @Post('/by-user-id')
+  async getPrivateMessages(userid_DTO: user_id_DTO): Promise<PrivateMessage[]> {
+    return await this.messageService.getPrivateMessagesByUserID(
+      userid_DTO.user_id,
+    );
+  }
 }
--- postgres-server/src/providers/chat/private/private-message.service.ts
+++ postgres-server/src/providers/chat/private/private-message.service.ts
@@ -9,4 +9,29 @@ export class PrivateMessageService {
     @InjectRepository(PrivateMessage)
     private messageRepository: Repository<PrivateMessage>,
   ) {}
+
+  async getPrivateMessagesByUserID(user_id: number): Promise<PrivateMessage[]> {
+    const tryFindBySenderID = await this.messageRepository.find({
+      relations: ['sender_id'],
+      where: {
+        sender_id: {
+          user_id: user_id,
+        },
+      },
+    });
+
+    if (tryFindBySenderID == undefined) {
+      const tryFindByReceiverID = await this.messageRepository.find({
+        relations: ['receiver_id'],
+        where: {
+          receiver_id: {
+            user_id: user_id,
+          },
+        },
+      });
+      return tryFindByReceiverID;
+    } else {
+      return tryFindBySenderID;
+    }
+  }
 }
